eba8053e5ee789e93c224423e0c687b73e6880f3
naveenk7 <k.naveen007@gmail.com>
2016-09-25 15:33:42 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
+
+//move the image
+var img = document.getElementById("madi");
+img.onclick = function() {
+    img.style.marginleft = '100px';
+    };
+    
