0d58746aead8757d153e55325e0166eadac6c79e
naveenk7 <k.naveen007@gmail.com>
2016-09-25 16:26:42 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,12 +7,12 @@ element.innerHTML = "New Value";
 //move the image
 var img = document.getElementById('madi');
 var marginLeft =0;
-function moveRight(){
+function moveRight () {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
     
 }
-img.onlcick= function(){
+img.onlcick= function () {
     var interval = setInterval(moveRight, 100); // moveRight every 100ms
 }
 
