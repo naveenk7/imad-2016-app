085fd5d2f64853a998ca8ffead50cc7e057695be
naveenk7 <k.naveen007@gmail.com>
2016-10-05 10:53:41 +1100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,28 @@ var counter=0;
 
 button.onclick = function() {
     
-    
-counter=counter+1;
-var span = document.getElementById('count');
+//Make a request to the counter end point
+var request = new XMLHttpRequest();
 
-span.innerHTML = counter.toString();
-    
+//capture the response and store it in a variable
+  request.onreadystatechange = function(){
+      if (request.readyState === XMLHttpRequest.DONE) {
+    // everything is good, the response is received
+                if (request.status === 200) {
+            // perfect!
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+            } 
+    } else {
+    // still not ready
+}
+      
+    //Make the request
+    request.open('GET', 'http://naveenk7.imad.hasura-app.io/counter', true);
+    request.send(null);
+  };
+
 }
 
 //change the text of the div main-text div
