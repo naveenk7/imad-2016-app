2c61a102fc869c621324a3ae8812ad1760690884
naveenk7 <k.naveen007@gmail.com>
2016-10-04 18:31:58 +1100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,6 @@ function moveRight () {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
  }
- 
-img.onlcick = function () {
-    var interval = setInterval(moveRight, 50); // moveRight every 100ms
+ img.onlcick = function () {
+    var interval = setInterval(moveRight, 50); 
 };
