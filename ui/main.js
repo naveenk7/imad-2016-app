2cf6e2983d928aaf696186b02b68088176b9d8d4
naveenk7 <k.naveen007@gmail.com>
2016-10-05 10:02:01 +1100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = "New Naveen";
 //move the image
 var img = document.getElementById('madi');
 var marginLeft = 0;
-function moveRight () {
+function moveRight() {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
  }
