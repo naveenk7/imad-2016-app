469f63a662a9f783d0aefc6a58877b50164998e1
naveenk7 <k.naveen007@gmail.com>
2016-10-04 18:35:45 +1100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
 //change the text of the div main-text div
 
-var element = document.getElementById("main-text");
+var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
 
 //move the image
-var img = document.getElementById("madi");
+var img = document.getElementById('madi');
 var marginLeft =0;
 function moveRight() {
     marginLeft = marginLeft + 10;
