77dd831a53ef28f62ff3dcefb74d4c002f388902
naveenk7 <k.naveen007@gmail.com>
2016-10-05 11:53:25 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,8 +107,8 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name', function (req, res) {
-  var name = req.params.name;
+app.get('/submit-name', function (req, res) { // URL /submit-name?name=xxxxx
+  var name = req.query.name;
   names.push(name);
   res.send(JSON.stringify(names));
 });
